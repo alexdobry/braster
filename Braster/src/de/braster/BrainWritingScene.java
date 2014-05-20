0497d4979e73115c4e7c7cba1854d1e8c46c97c4
Patrick Englert <englert.p@gmx.de>
2014-05-20 11:08:22 +0200

    .

--- Braster/src/de/braster/BrainWritingScene.java
+++ Braster/src/de/braster/BrainWritingScene.java
@@ -194,7 +194,7 @@ public class BrainWritingScene extends AbstractScene{
 		getCanvas().addChild(keyboard);
 		
 		keyboard.scale(0.8f, 0.8f, 1, new Vector3D(0, 0, 0));
-		keyboard.removeAllGestureEventListeners(DragProcessor.class);
+		//keyboard.removeAllGestureEventListeners(DragProcessor.class);
 		keyboard.removeAllGestureEventListeners(ScaleProcessor.class);
 		keyboard.removeAllGestureEventListeners(RotateProcessor.class);
 		
