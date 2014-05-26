0593c9c99f0a4bf9aa5627407c42ed0a79a6fb5f
Patrick Englert <englert.p@gmx.de>
2014-05-26 13:25:24 +0200

    initial

---
+++ Braster2/src/de/braster/ClusteringScene.java
@@ -0,0 +1,266 @@
+package de.braster;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+import org.mt4j.MTApplication;
+import org.mt4j.components.MTCanvas;
+import org.mt4j.components.visibleComponents.font.FontManager;
+import org.mt4j.components.visibleComponents.font.IFont;
+import org.mt4j.components.visibleComponents.shapes.MTRoundRectangle;
+import org.mt4j.components.visibleComponents.widgets.MTTextArea;
+import org.mt4j.components.visibleComponents.widgets.MTTextField;
+import org.mt4j.components.visibleComponents.widgets.buttons.MTImageButton;
+import org.mt4j.input.gestureAction.DefaultButtonClickAction;
+import org.mt4j.input.inputProcessors.IGestureEventListener;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProcessor;
+import org.mt4j.sceneManagement.AbstractScene;
+import org.mt4j.sceneManagement.Iscene;
+import org.mt4j.util.MTColor;
+import org.mt4j.util.math.Vector3D;
+import org.mt4jx.components.visibleComponents.widgets.MTCheckbox;
+
+import processing.core.PImage;
+
+public class ClusteringScene extends AbstractScene{
+
+	
+	private MTCanvas canv;
+	private MTApplication mtApp;
+	private Iscene evaluationScene;
+	private LinkedList<Idea> ideas;
+	
+	public ClusteringScene( MTApplication mtApplication, String name) 
+	{
+		super(mtApplication, name);
+		
+		this.mtApp = mtApplication;		
+		this.canv = getCanvas();
+		//setzt Hintergrundfarbe
+		this.setClearColor(MTColor.WHITE);
+		
+		ideas = Idea.getAllIdeas();
+		float posx = 0;
+		float posy = 0;
+		
+		float padding = 20;
+		
+		Random rng = new Random();
+		for (Idea idea : ideas) {
+			canv.addChild(idea);
+			posy = rng.nextFloat() * (mtApp.height-padding*2);
+			posx = rng.nextFloat() * (mtApp.width-padding*2);
+			idea.setPositionGlobal(new Vector3D(posx+padding, posy+padding));
+			idea.updateCanvas(canv); //TODO:
+		}
+		
+		
+		
+//		createItem();
+//		createLeftMenubar();
+		createRightMenubar();
+	}
+
+	
+	
+	//create left menubar for options
+	private void createLeftMenubar()
+	{		
+		final MTRoundRectangle mtRoundRectangle = new MTRoundRectangle(this.mtApp, 20,this.mtApp.height-180, 0, 200, 160, 12, 12);
+		mtRoundRectangle.unregisterAllInputProcessors();
+		mtRoundRectangle.setFillColor(MTColor.GREY); 	
+		mtRoundRectangle.registerInputProcessor(new TapProcessor(this.mtApp));
+		
+		final MTCheckbox checkBox1 = new MTCheckbox(this.mtApp, 20);
+		checkBox1.setFillColor(MTColor.WHITE);		 
+        checkBox1.setBooleanValue(false);
+        checkBox1.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+            
+            @Override
+            public boolean processGestureEvent(MTGestureEvent ge) {
+                if(checkBox1.getBooleanValue()){
+                    
+                }
+                else{
+                   
+                }
+                return false;
+            }
+        });
+ 
+		
+		checkBox1.setPositionRelativeToParent(new Vector3D(50,this.mtApp.height-150));
+		mtRoundRectangle.addChild(checkBox1);
+		
+		
+		final MTCheckbox checkBox2 = new MTCheckbox(this.mtApp, 20);
+		checkBox2.setFillColor(MTColor.WHITE);
+        checkBox2.setBooleanValue(true);
+        checkBox2.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+            
+            @Override
+            public boolean processGestureEvent(MTGestureEvent ge) {
+                if(checkBox2.getBooleanValue()){
+                    
+                }
+                else{
+                   
+                }
+                return false;
+            }
+        });
+ 
+		
+		checkBox2.setPositionRelativeToParent(new Vector3D(50,this.mtApp.height-105));
+		mtRoundRectangle.addChild(checkBox2);
+		
+		
+		final MTCheckbox checkBox3 = new MTCheckbox(this.mtApp, 20);
+		checkBox3.setFillColor(MTColor.WHITE);
+        checkBox3.setBooleanValue(true);
+        checkBox3.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+            
+            @Override
+            public boolean processGestureEvent(MTGestureEvent ge) {
+                if(checkBox3.getBooleanValue()){
+                    
+                }
+                else{
+                   
+                }
+                return false;
+            }
+        });
+ 
+		
+		checkBox3.setPositionRelativeToParent(new Vector3D(50,this.mtApp.height-60));
+		mtRoundRectangle.addChild(checkBox3);
+			
+		 
+		IFont font = FontManager.getInstance().createFont(this.mtApp, "arial.ttf", 
+				15, MTColor.WHITE, false);
+		
+        final MTTextArea textArea1 = new MTTextArea(this.mtApp,font);
+        textArea1.setText("show tips");
+        textArea1.setFillColor(MTColor.GREY);
+        textArea1.setStrokeColor(MTColor.GREY);
+        textArea1.setPositionRelativeToParent(new Vector3D(120,this.mtApp.height-150));
+        mtRoundRectangle.addChild(textArea1);
+		
+        final MTTextArea textArea2 = new MTTextArea(this.mtApp,font);
+        textArea2.setText("show sorted ideas");
+        textArea2.setFillColor(MTColor.GREY);
+        textArea2.setStrokeColor(MTColor.GREY);
+        textArea2.setPositionRelativeToParent(new Vector3D(140,this.mtApp.height-105));
+        mtRoundRectangle.addChild(textArea2);
+        
+        final MTTextArea textArea3 = new MTTextArea(this.mtApp,font);
+        textArea3.setText("Anleitung");
+        textArea3.setFillColor(MTColor.GREY);
+        textArea3.setStrokeColor(MTColor.GREY);
+        textArea3.setPositionRelativeToParent(new Vector3D(120,this.mtApp.height-60));
+        mtRoundRectangle.addChild(textArea3);
+		
+		this.canv.addChild(mtRoundRectangle);	
+		
+
+		
+		//3 Radiobuttons mit jeweils label dahinter
+	
+	}
+	
+	
+	//create right menubar, um mit der nächsten Scene zu beginnen
+	private void createRightMenubar()
+	{		
+		final MTRoundRectangle mtRoundRectangle = new MTRoundRectangle(this.mtApp, this.mtApp.width-220, this.mtApp.height-80, 0, 200, 60, 12, 12);
+		mtRoundRectangle.unregisterAllInputProcessors();
+		mtRoundRectangle.setFillColor(MTColor.GREY);  	
+		mtRoundRectangle.registerInputProcessor(new TapProcessor(this.mtApp));
+		mtRoundRectangle.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+			public boolean processGestureEvent(MTGestureEvent ge) {
+				TapEvent te = (TapEvent)ge;
+				if (te.isTapped()){
+										
+					//Save the current scene on the scene stack before changing
+					mtApp.pushScene();
+					if (evaluationScene == null){
+						evaluationScene = new EvaluationScene(mtApp, "Evaluation");
+						//Konstruktor erweitern um Anzahl Spieler, da genau soviele
+						//Tastaturen geladen werden
+					//Add the scene to the mt application
+					mtApp.addScene(evaluationScene);
+					}
+					//Do the scene change
+					mtApp.changeScene(evaluationScene);
+				}
+				return false;
+			}
+		});
+		
+		IFont font = FontManager.getInstance().createFont(this.mtApp, "arial.ttf", 
+        		20, MTColor.WHITE, false);
+		
+		MTTextArea rText = new MTTextArea(this.mtApp, font);
+		rText.unregisterAllInputProcessors();
+		rText.setPickable(false);
+		rText.setNoFill(true);
+		rText.setNoStroke(true);
+		rText.setText("             Go to \n Evaluation Scene");
+		mtRoundRectangle.addChild(rText);
+		rText.setPositionRelativeToParent(mtRoundRectangle.getCenterPointLocal());
+		
+		this.canv.addChild(mtRoundRectangle);	
+	}
+	
+	private void createItem()
+	{
+		IFont font = FontManager.getInstance().createFont(this.mtApp, "arial.ttf", 
+        		30, MTColor.BLACK, false);
+		
+		final MTRoundRectangle mtRoundRectangle = new MTRoundRectangle(this.mtApp, 400, 400, 0, 300, 50, 12, 12);
+		mtRoundRectangle.unregisterAllInputProcessors();
+	//	mtRoundRectangle.setFillColor(MTColor.BLUE);
+		mtRoundRectangle.setStrokeColor(MTColor.BLUE);
+
+		//Bild als Hintergrund eines Rectangle haben
+		// final PImage eraser = this.mtApp.loadImage("C:\\Users\\Stefan\\Desktop\\notizzettel.jpg");
+	    //mtRoundRectangle.setTexture(eraser);
+	
+	      /*
+		MTTextArea rText = new MTTextArea(this.mtApp, font);
+		rText.unregisterAllInputProcessors();
+		rText.setPickable(false);
+		rText.setNoFill(true);
+		rText.setNoStroke(true);
+		rText.setText("hier steht was");
+		mtRoundRectangle.addChild(rText);
+		rText.setPositionRelativeToParent(mtRoundRectangle.getCenterPointLocal());
+	*/
+		mtRoundRectangle.registerInputProcessor(new TapProcessor(getMTApplication()));
+		mtRoundRectangle.addGestureListener(TapProcessor.class, new DefaultButtonClickAction(mtRoundRectangle));
+		mtRoundRectangle.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+			public boolean processGestureEvent(MTGestureEvent ge) {
+				TapEvent te = (TapEvent)ge;
+				if (te.isTapped()){
+										
+					//entweder wird die idee ausgeklappt
+					//oder wieder eingeklappt
+					//je nach vorherigem status
+					//höhe nach text ausrichten
+					//dazu ne methode schreiben
+					
+					mtRoundRectangle.setSizeLocal(300, 500);
+				}
+				return false;
+			}
+		});
+		this.canv.addChild(mtRoundRectangle);
+		
+	}
+}
---
+++ Braster2/src/de/braster/StartBraster.java
@@ -0,0 +1,20 @@
+package de.braster;
+import org.mt4j.MTApplication;
+
+
+
+
+public class StartBraster extends MTApplication {
+	
+	
+	public static void main(String[] args) {
+		initialize();
+	}
+ 
+	@Override
+	public void startUp() {
+		addScene(new SetupScene(this, "Hello World Scene"));
+		//addScene(new BrainWritingScene(this, "Hello World Scene"));
+	}
+}
+
