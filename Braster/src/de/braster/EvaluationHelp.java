e4e8379f9c0713264507d249594fd40cf5dbbcaf
stefan <Julia@Julia-PC>
2014-06-15 14:50:18 +0200

    add "backgroundimage" add "class Ohne Funktionalität"

---
+++ Braster/src/de/braster/EvaluationHelp.java
@@ -0,0 +1,21 @@
+package de.braster;
+
+import org.mt4j.MTApplication;
+import org.mt4j.sceneManagement.AbstractScene;
+
+public class EvaluationHelp extends AbstractScene {
+
+	public EvaluationHelp(MTApplication mtApplication, String name) {
+		super(mtApplication, name);
+		
+		//geblurrte Bild als Hintergrund einstellen
+		
+		//Textareas im Tabellenkopf adden
+		
+		//Mitte eine Box erstellen mit dem Helptext und den Animationen,
+		//wo die möglichen Aktionen vorgeführt werden
+		
+		//wenn ins Bild getippt wird, startet die normale EvaluationScene
+	}
+
+}
